d9da99fa4942c2728325937ba6424b03eb36b3d5
Zeejfps@gmail.com <Zeejfps@gmail.com>
2024-07-09 17:46:41 -0500

    Fixed broken ZLibStream encoding

--- PngSharp/Api/PngApi.cs
+++ PngSharp/Api/PngApi.cs
@@ -3,14 +3,14 @@ using PngSharp.Encoder;
 
 namespace PngSharp.Api;
 
-public static class PngApi
+public sealed class PngApi
 {
     /// <summary>
     /// Decodes a PNG image from a file
     /// </summary>
     /// <param name="pathToFile"></param>
     /// <returns>Decoded PNG image containing information about the image and its pixel data</returns>
-    public static IDecodedPng DecodeFromFile(string pathToFile)
+    public IDecodedPng DecodeFromFile(string pathToFile)
     {
         using var fileStream = new FileStream(pathToFile, FileMode.Open);
         return DecodeFromStream(fileStream);
@@ -21,7 +21,7 @@ public static class PngApi
     /// </summary>
     /// <param name="stream"></param>
     /// <returns>Decoded PNG image containing information about the image and its pixel data</returns>
-    public static IDecodedPng DecodeFromStream(Stream stream)
+    public IDecodedPng DecodeFromStream(Stream stream)
     {
         using var decoder = new PngDecoder(stream);
         decoder.Decode();
@@ -39,13 +39,13 @@ public static class PngApi
         return decodedPng;
     }
 
-    public static void EncodeToFile(IDecodedPng decodedPng, string pathToFile)
+    public void EncodeToFile(IDecodedPng decodedPng, string pathToFile)
     {
         using var fileStream = new FileStream(pathToFile, FileMode.Create);
         EncodeToStream(decodedPng, fileStream);
     }
 
-    public static void EncodeToStream(IDecodedPng decodedPng, Stream stream)
+    public void EncodeToStream(IDecodedPng decodedPng, Stream stream)
     {
         using var encoder = new PngEncoder(decodedPng, stream);
         encoder.Encode();
--- PngSharpTests/Program.cs
+++ PngSharpTests/Program.cs
@@ -1,29 +1,5 @@
-﻿using System.Text;
-using PngSharp.Api;
+﻿using PngSharp.Api;
 
-var decodedPng = PngApi.DecodeFromFile("Assets/sprite_atlas_128x64.png");
-
-PngApi.EncodeToFile(decodedPng, "test_64x64.png");
-
-Console.WriteLine(decodedPng.ColorType);
-
-SaveToPAM("test.pam", decodedPng.PixelData, decodedPng.Width, decodedPng.Height);
-
-void SaveToPAM(string filename, byte[] rgbaData, int width, int height)
-{
-    using (FileStream fs = new FileStream(filename, FileMode.OpenOrCreate))
-    using (BinaryWriter bw = new BinaryWriter(fs))
-    {
-        // Write PAM header
-        bw.Write(Encoding.ASCII.GetBytes("P7\n"));
-        bw.Write(Encoding.ASCII.GetBytes($"WIDTH {width}\n"));
-        bw.Write(Encoding.ASCII.GetBytes($"HEIGHT {height}\n"));
-        bw.Write(Encoding.ASCII.GetBytes("DEPTH 4\n"));
-        bw.Write(Encoding.ASCII.GetBytes("MAXVAL 255\n"));
-        bw.Write(Encoding.ASCII.GetBytes("TUPLTYPE RGB_ALPHA\n"));
-        bw.Write(Encoding.ASCII.GetBytes("ENDHDR\n"));
-
-        // Write image data
-        bw.Write(rgbaData);
-    }
-}
+var pngApi = new PngApi();
+var decodedPng = pngApi.DecodeFromFile("Assets/sprite_atlas_128x64.png");
+pngApi.EncodeToFile(decodedPng, "test_64x64.png");
