3b24f3c0b7c6baa69f66136ee5bedd7fba952a56
Zeejfps@gmail.com <Zeejfps@gmail.com>
2024-07-10 17:04:09 -0500

    Cleaned up the PaethTypeFilter

--- PngSharp/Spec/AdaptiveFilter/Types/PaethTypeFilter.cs
+++ PngSharp/Spec/AdaptiveFilter/Types/PaethTypeFilter.cs
@@ -5,49 +5,38 @@ internal class PaethTypeFilter : FilterBase
     public PaethTypeFilter(int bytesPerPixel) : base(bytesPerPixel) { }
 
     public override AdaptiveFilterTypeKind Kind => AdaptiveFilterTypeKind.Paeth;
+    
     protected override byte ComputeValue(ReadOnlySpan<byte> currentRow, ReadOnlySpan<byte> previousRowBuffer, int currentIndex)
     {
         var x = currentRow[currentIndex];
         var left = GetLeftValue(currentRow, currentIndex);
         var above = GetAboveValue(previousRowBuffer, currentIndex);
         var aboveLeft = GetAboveLeftByteValue(previousRowBuffer, currentIndex);
-        //return (byte)(x - PaethPredictor(a, b, c));
-
-        int p = left + above - aboveLeft;
-        int pa = Math.Abs(p - left);
-        int pb = Math.Abs(p - above);
-        int pc = Math.Abs(p - aboveLeft);
-        
-        if (pa <= pb && pa <= pc)
-            return (byte)((x - left) & 0xFF);
-        if (pb <= pc)
-            return (byte)((x - above) & 0xFF);
-        
-        return (byte)((x - aboveLeft) & 0xFF);
+        return (byte)(x - PaethPredictor(left, above, aboveLeft));
     }
 
     protected override byte ReverseValue(ReadOnlySpan<byte> currentRow, ReadOnlySpan<byte> prevRow, int currByteIndex)
     {
         var x = currentRow[currByteIndex];
-        var a = GetLeftValue(currentRow, currByteIndex);
-        var b = GetAboveValue(prevRow, currByteIndex);
-        var c = GetAboveLeftByteValue(prevRow, currByteIndex);
-        return (byte)(x + PaethPredictor(a, b, c));
+        var left = GetLeftValue(currentRow, currByteIndex);
+        var above = GetAboveValue(prevRow, currByteIndex);
+        var aboveLeft = GetAboveLeftByteValue(prevRow, currByteIndex);
+        return (byte)(x + PaethPredictor(left, above, aboveLeft));
     }
 
-    private byte PaethPredictor(byte left, byte up, byte upLeft)
+    private byte PaethPredictor(byte left, byte above, byte aboveLeft)
     {
-        var p = left + up - upLeft;
+        var p = left + above - aboveLeft;
         var pa = Math.Abs(p - left);
-        var pb = Math.Abs(p - up);
-        var pc = Math.Abs(p - upLeft);
+        var pb = Math.Abs(p - above);
+        var pc = Math.Abs(p - aboveLeft);
 
         if (pa <= pb && pa <= pc)
             return left;
         
         if (pb <= pc)
-            return up;
+            return above;
         
-        return upLeft;
+        return aboveLeft;
     }
 }
