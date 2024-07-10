2c7060064b84942f2444e49f7ea7e26f7b259e6d
Zeejfps@gmail.com <Zeejfps@gmail.com>
2024-07-10 17:29:24 -0500

    Moved the readme; Added the ILogger inerface

---
+++ PngSharp/Api/ILogger.cs
@@ -0,0 +1,9 @@
+﻿namespace PngSharp.Api;
+
+public interface ILogger
+{
+    void Debug(string message);
+    void Info(string message);
+    void Warning(string message);
+    void Error(string message);
+}
