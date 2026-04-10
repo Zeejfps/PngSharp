249b00868facfcaab02ccf42e784ea641a4c82c2
zeejfps <zeejfps@gmail.com>
2026-04-10 09:33:37 -0500

    fix: Made ChunkHeader protected
    This SHOULD be a breaking change...

--- PngSharp/Spec/ChunkHeader.cs
+++ PngSharp/Spec/ChunkHeader.cs
@@ -1,12 +1,7 @@
 ﻿namespace PngSharp.Spec;
 
-public readonly struct ChunkHeader
+internal readonly record struct ChunkHeader
 {
     public int ChunkSizeInBytes { get; init; }
     public string Id { get; init; }
-
-    public override string ToString()
-    {
-        return $"{nameof(ChunkSizeInBytes)}: {ChunkSizeInBytes}, {nameof(Id)}: {Id}";
-    }
 }
